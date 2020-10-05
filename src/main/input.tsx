3823cba235bb74978dabc2334533e44bee4f833c
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2020-10-05 22:47:17 +0300

    emotion css-in-js setup

--- src/main/input.tsx
+++ src/main/input.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 type Props = {
   userInput: string;
-  handleUserInput: () => {};
+  saveInput: () => {};
 };
 
-const Input: React.FunctionComponent<Props> = ({ userInput, handleUserInput }) => (
+const Input: React.FunctionComponent<Props> = ({ userInput, saveInput }) => (
   <div css={inputCss}>
     <h2>User Input</h2>
     <textarea
       placeholder="Start typing..."
       css={textAreaCss}
       value={userInput}
-      onChange={handleUserInput}
+      onChange={saveInput}
     />
   </div>
 );
@@ -22,4 +22,6 @@ export default Input;
 
 const inputCss = css``;
 
-const textAreaCss = css``;
+const textAreaCss = css`
+  resize: none;
+`;
