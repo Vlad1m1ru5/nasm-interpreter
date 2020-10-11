795a8a52280957dffc534d0520115d1bdae85a5c
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2020-10-11 18:57:29 +0300

    input resize

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,17 +1,39 @@
 import { css, Global } from "@emotion/core";
-import React from "react";
+import React, { useState } from "react";
 
-import EditorLayout from "./editor-layout";
-import Input from "./input";
+interface Props {
+  executeCode: (code: String) => {};
+};
 
-const Main = () => (
-  <>
-    <EditorLayout>
-      <Input />
-    </EditorLayout>
-    <Global styles={globalStyles} />
-  </>
-);
+const Main: React.FunctionComponent<Props> = ({ executeCode }) => {
+
+  const [userInput, setUserInput] = useState("");
+
+  const stageUserInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    event.persist();
+    setUserInput(currentUserInput => event.target.value);
+  };
+
+  const processUserInput = () => {
+    executeCode(userInput);
+  };
+
+  return (
+    <>
+      <h1>NASM Web Interpreter</h1>
+      <h2>User Input</h2>
+      <textarea
+        css={textAreaCss}
+        placeholder="Start typing..."
+        onChange={stageUserInput}
+      />
+      <div>
+        <button onClick={processUserInput}>Сделать шаг</button>
+      </div>
+      <Global styles={globalStyles} />
+    </>
+  );
+};
 
 export default Main;
 
@@ -22,6 +44,15 @@ const globalStyles = css`
 
   body {
     margin: 0;
-    height: 100vh;
+    padding-left: 300px;
+    padding-right: 300px;
   }
+`;
+
+const textAreaCss = css`
+  resize: none;
+  font-size: inherit;
+  font-family: inherit;
+  width: 100%;
+  height: 500px;
 `;
