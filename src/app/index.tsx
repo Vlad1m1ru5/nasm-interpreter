0eb074e1f8c668df36369955ae19bb8dae0de091
Vlad1m1ru5 <kubantsev.vov@mail.ru>
2020-10-18 20:42:51 +0300

    text area half size

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
-import React, { TextareaHTMLAttributes, useState } from 'react'
+import { css, Global } from '@emotion/core';
+import React, { useState } from 'react'
 
 const App: React.FunctionComponent = () => {
 
@@ -7,14 +8,37 @@ const App: React.FunctionComponent = () => {
   const asmCodeOnChange = ({ target }:  React.ChangeEvent<HTMLTextAreaElement>) => {
     const asmCode = target.value;
 
-    setAsmCode(asmCode);
+    setAsmCode((asmCurrentCode) => asmCode);
   }
 
   return (
     <>
-      <textarea placeholder='Код ассемблера...' onChange={asmCodeOnChange}/>
+      <textarea 
+        css={textAreaCss} 
+        onChange={asmCodeOnChange} 
+        placeholder='Код ассемблера...' 
+      />
+        <Global styles={globalCss}/>
     </>
   )
 }
 
 export default App
+
+const textAreaCss = css`
+  resize: none;
+  width: 50%;
+  height: 50%;
+`
+
+const globalCss = css`
+  body {
+    margin: 0;
+    height: 100vh;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+  }
+`
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import App from 'app'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+)
